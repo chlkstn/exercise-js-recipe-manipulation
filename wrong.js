5aef4eb38b3b87764b7b4ba326914527d5124588
chlkstn <chalkstoneinteractive@gmail.com>
2024-07-11 16:55:06 +0200

    staring part 2

---
+++ wrong.js
@@ -0,0 +1,17 @@
+/* 1 */
+document.querySelector(".logo-text").style.color = "black";
+
+/* 2 */
+
+document.querySelector("header").style.justifyContent = "flex-start";
+
+/* 3 */
+
+document.querySelector("header").style.borderColor = "black";
+
+/*4 */
+
+document.getElementById("recipe-name").textContent = "Frozen Cheesecake";
+
+
+
