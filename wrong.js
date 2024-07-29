564831683648ac62ed2619333d3b8e6c31bb5b9b
chlkstn <chalkstoneinteractive@gmail.com>
2024-07-29 09:02:01 +0200

    updating excerise

--- wrong.js
+++ wrong.js
@@ -13,5 +13,55 @@ document.querySelector("header").style.borderColor = "black";
 
 document.getElementById("recipe-name").textContent = "Frozen Cheesecake";
 
+/*5 */
 
+let newTime = document.querySelector(".time-container > span");
 
+newTime.classList.add("material-icons");
+
+/*6 */
+
+document.querySelector(".time").textContent = "60+  min";
+
+/*7 */
+
+document.querySelector(".image-container > img").src =
+  "/assets/frozen-cheesecake-slice.jpg";
+
+/*8 */
+
+document.querySelector(".ingredients-container").style.backgroundColor =
+  "#f9f9f9";
+
+/*9 10 11*/
+
+/* Övre*/
+
+let newl2 = document.createElement("li");
+
+let newl1 = document.createElement("li");
+
+newl1.textContent = "15st digestivekex";
+
+newl2.textContent = "lite smör";
+
+document.querySelector(".ingredients-list-bottom").appendChild(newl1);
+document.querySelector(".ingredients-list-bottom").appendChild(newl2);
+document.querySelector(".ingredients-list-bottom > p").remove("p");
+
+/* Undre */
+
+let newl3 = document.createElement("li");
+
+newl3.textContent = "400g naturell philadelphiaost";
+
+document.querySelector(".ingredients-list-paste>:nth-child(3)").textContent =
+  "3tsk vaniljsocker";
+
+document.querySelector(".ingredients-list-paste").appendChild(newl3);
+
+/*12 */
+
+document.querySelector(".shadow").style.boxShadow = "0px 0px 0px ";
+
+/*13 */
